c462c4aba77c6cfb5c2f11f36d13546d43d55268
Michael Babker <mbabker@flbab.com>
2015-09-10 12:10:14 -0400

    Initialize generator bundle

---
+++ MauticGeneratorBundle.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @package     Mautic
+ * @copyright   2015 Mautic Contributors. All rights reserved.
+ * @author      Mautic
+ * @link        http://mautic.org
+ * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+namespace Mautic\Bundle\MauticGeneratorBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * MauticGeneratorBundle
+ */
+class MauticGeneratorBundle extends Bundle
+{
+    /**
+     * {@inheritdoc}
+     *
+     * @return string The Bundle parent name it overrides or null if no parent
+     */
+    public function getParent()
+    {
+		return 'SensioGeneratorBundle';
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+Mautic Generator Bundle
+===========
+<img src="https://www.mautic.org/media/images/github_readme.png" />
+<br /><br />
+## Getting Started
+<p>
+	The MauticGeneratorBundle is a developer tool used for helping to build bundles for Mautic. 
+</p>
